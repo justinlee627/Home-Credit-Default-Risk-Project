3bf4382650e44ddbbc2b9d453c6e5deb70e38550
Justin Lee <justinl2@andrew.cmu.edu>
2018-06-19 22:12:47 -0400

    Add files via upload

---
+++ datasetup.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd 
+
+
+app_test = pd.read_csv("application_test.csv")
+app_train = pd.read_csv("application_train.csv")
+bureau = pd.read_csv("bureau.csv")
+bureau_bal = pd.read_csv("bureau_balance.csv")
+cc_balance = pd.read_csv("credit_card_balance.csv")
+install_payments = pd.read_csv("installments_payments.csv")
+PC_balance = pd.read_csv("POS_CASH_balance.csv")
+prev_app = pd.read_csv("previous_application.csv")
+sample_sub = pd.read_csv("sample_submission.csv")
+
+
+
+
